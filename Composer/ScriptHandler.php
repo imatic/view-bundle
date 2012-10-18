f95a92426e2bdceca90d3a6eca80b2d357b62f47
Stepan Koci <stepan.koci@imatic.cz>
2012-10-18 18:06:37 +0200

    Added script handler for assetic dump

---
+++ Composer/ScriptHandler.php
@@ -0,0 +1,55 @@
+<?php
+
+/*
+ * Downloaded from https://gist.github.com/2725096
+ * More info: https://github.com/symfony/AsseticBundle/issues/82 (but it's still not in assetic or distribution bundle)
+ */
+
+namespace Imatic\Bundle\ViewBundle\Composer;
+
+use Symfony\Component\Process\PhpExecutableFinder;
+use Symfony\Component\Process\Process;
+
+class ScriptHandler
+{
+    public static function dumpAssets($event)
+    {
+        $options = self::getOptions($event);
+        $appDir = $options['symfony-app-dir'];
+
+        $arguments = array();
+
+        if ($options['assetic-dump-force'])
+            $arguments[] = '--force';
+
+        if ($options['assetic-dump-asset-root'] !== null)
+            $arguments = escapeshellarg($options['assetic-dump-asset-root']);
+
+        static::executeCommand($event, $appDir, 'assetic:dump' . implode(' ', $arguments));
+    }
+
+    protected static function executeCommand($event, $appDir, $cmd)
+    {
+        $phpFinder = new PhpExecutableFinder;
+        $php = escapeshellarg($phpFinder->find());
+        $console = escapeshellarg($appDir . '/console');
+        if ($event->getIO()->isDecorated()) {
+            $console .= ' --ansi';
+        }
+
+        $process = new Process($php . ' ' . $console . ' ' . $cmd);
+        $process->run(function ($type, $buffer) use ($event) {
+            $event->getIO()->write($buffer, false);
+        });
+    }
+
+    protected static function getOptions($event)
+    {
+        $options = array_merge(array(
+            'assetic-dump-asset-root' => null,
+            'assetic-dump-force' => false,
+        ), $event->getComposer()->getPackage()->getExtra());
+
+        return $options;
+    }
+}
