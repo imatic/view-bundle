248a2c2c026006c5417b9aa8d2629158dcc67194
Stepan Koci <stepan.koci@imatic.cz>
2014-02-19 19:55:29 +0100

    Table row actions.

--- Templating/Helper/Action/ActionHelper.php
+++ Templating/Helper/Action/ActionHelper.php
@@ -3,6 +3,8 @@
 namespace Imatic\Bundle\ViewBundle\Templating\Helper\Action;
 
 use Imatic\Bundle\ViewBundle\Templating\Helper\Html\HtmlElement;
+use Symfony\Component\PropertyAccess\PropertyAccess;
+use Symfony\Component\PropertyAccess\PropertyAccessor;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class ActionHelper
@@ -12,9 +14,15 @@ class ActionHelper
      */
     private $urlGenerator;
 
+    /**
+     * @var PropertyAccessor
+     */
+    private $accessor;
+
     public function __construct(UrlGeneratorInterface $urlGenerator)
     {
         $this->urlGenerator = $urlGenerator;
+        $this->accessor = PropertyAccess::createPropertyAccessor();
     }
 
     public function create($actionOptions, $contextOptions = null)
@@ -22,13 +30,17 @@ class ActionHelper
         if (!($actionOptions instanceof ActionOptions)) {
             $actionOptions = new ActionOptions($actionOptions);
         }
-        if (is_null($contextOptions) || is_array($contextOptions)) {
+        if (is_null($contextOptions) || !is_array($contextOptions)) {
             $contextOptions = [];
         }
 
         $url = $actionOptions->url;
         if (!$url && $actionOptions->route) {
-            $url = $this->urlGenerator->generate($actionOptions->route, $actionOptions->routeParams);
+            $routeParameters = $actionOptions->routeParams;
+            if (array_key_exists('item', $contextOptions)) {
+                $routeParameters = $this->applyParameters($contextOptions['item'], $routeParameters);
+            }
+            $url = $this->urlGenerator->generate($actionOptions->route, $routeParameters);
         }
 
         $element = new HtmlElement('a', $actionOptions->label, ['href' => $url]);
@@ -39,6 +51,19 @@ class ActionHelper
         $element->classes()->add($actionOptions->class);
 
         return $element;
+    }
+
+    public function applyParameters($object, array $parameters)
+    {
+        foreach ($parameters as $name => $value) {
+            if (0 === strpos($value, '#')) {
+                $key = substr($value, 1);
+                $value = $this->accessor->getValue($object, $key);
+                $parameters[$name] = $value;
+            }
+        }
+
+        return $parameters;
     }
 
     public function getActionOptions($action)
