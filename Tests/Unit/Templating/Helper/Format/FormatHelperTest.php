0826061288dae17e8b98de1dfffee61a157c4660
Miloslav Nenadal <miloslav.nenadal@imatic.cz>
2015-04-28 14:20:42 +0200

    RESEARCH-125: Nefunguje razeni s DBAL driverem

---
+++ Tests/Unit/Templating/Helper/Format/FormatHelperTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Imatic\Bundle\ViewBundle\Tests\Unit\Templating\Helper\Format;
+
+use Imatic\Bundle\ViewBundle\Templating\Helper\Format\FormatHelper;
+use PHPUnit_Framework_TestCase;
+use stdClass;
+
+class FormatHelperTest extends PHPUnit_Framework_TestCase
+{
+    private $formatHelper;
+
+    protected function setUp()
+    {
+        $container = $this->getMock('Symfony\Component\DependencyInjection\ContainerInterface');
+
+        $formatter = $this->getMock('Imatic\Bundle\ViewBundle\Templating\Helper\Format\FormatterInterface');
+        $formatter->expects($this->any())
+            ->method('format')
+            ->willReturnCallback(function ($value) {
+                return $value;
+            });
+
+        $this->formatHelper = new FormatHelper($container);
+        $this->formatHelper->addFormatter('text', $formatter);
+    }
+
+    public function testRenderValueShouldWorkWithArrays()
+    {
+        $data = [
+            'name' => 'jmeno',
+            'surname' => 'prijmeni'
+        ];
+
+        $this->assertEquals('jmeno', $this->formatHelper->renderValue($data, 'name'));
+        $this->assertEquals('jmeno', $this->formatHelper->renderValue($data, '[name]'));
+    }
+
+    public function testRenderValueShouldWorkWithObjects()
+    {
+        $data = new stdClass();
+        $data->name = 'jmeno';
+        $data->surname = 'prijmeni';
+
+        $this->assertEquals('jmeno', $this->formatHelper->renderValue($data, 'name'));
+    }
+}
