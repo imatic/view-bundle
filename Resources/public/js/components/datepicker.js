cdcb9ac1dfa2a67b88038b4ec8d0250e92f63d9b
Pavel Batecko <pavel.batecko@imatic.cz>
2015-05-29 18:20:01 +0200

    Updated datepicker handling (MopaBootstrapBundle v3.0.0-beta4)

---
+++ Resources/public/js/components/datepicker.js
@@ -0,0 +1,57 @@
+"use_strict";
+
+var Imatic;
+(function (Imatic) {
+    (function (View) {
+        (function (Datepicker) {
+
+            // https://github.com/smalot/bootstrap-datetimepicker#options
+            Datepicker.defaults = {
+                base: {
+                    autoclose: true,
+                    pickerPosition: 'bottom-left',
+                    todayBtn: true
+                },
+                datepicker: {
+                    format: 'dd/mm/yyyy',
+                    minView: 'month',
+                    startView: 'month'
+                },
+                datetimepicker: {
+                    format: 'dd/mm/yyyy hh:ii'
+                },
+                timepicker: {
+                    format: 'hh:ii',
+                    formatViewType: 'time',
+                    maxView: 'day',
+                    minView: 'hour',
+                    startView: 'day'
+                }
+            };
+
+            // https://github.com/naitsirch/mopa-bootstrap-bundle/blob/master/Resources/doc/3.3-form-components.md
+            Datepicker.init = function (id, language) {
+                var $elem = $('#' + id).parent('div.date');
+
+                if ($elem.length < 1) {
+                    throw new Error('Datepicker #' + id + ' was not found');
+                }
+
+                var options = $.extend(
+                    {language: language},
+                    Datepicker.defaults.base,
+                    Datepicker.defaults[$elem.data('provider')]
+                );
+
+                $elem.datetimepicker(options);
+                
+                $elem.find('input[type=hidden]').each(function () {
+                    if ($(this).val()) {
+                        $(this).parent().datetimepicker('setValue');
+                    }
+                });
+            };
+
+        })(View.Datepicker || (View.Datepicker = {}));
+    })(Imatic.View || (Imatic.View = {}));
+})(Imatic || (Imatic = {}));
