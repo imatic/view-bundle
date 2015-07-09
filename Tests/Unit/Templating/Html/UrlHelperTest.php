543db51a1ea796af1478dea10ef33f4cd532fe5e
Pavel Batecko <pavel.batecko@imatic.cz>
2015-07-09 14:56:00 +0200

    RESEARCH-138 - Aktualizace testu a kodu pro SF 2.6+

--- Tests/Unit/Templating/Html/UrlHelperTest.php
+++ Tests/Unit/Templating/Html/UrlHelperTest.php
@@ -5,11 +5,11 @@ use Imatic\Bundle\ViewBundle\Templating\Helper\Html\UrlHelper;
 
 class UrlHelperTest extends \PHPUnit_Framework_TestCase
 {
-    private $testUrl = 'https://www.example.com/path/file.html?param1=value1&param2[sub1]=valueSub1&param2[sub2]=valueSub2';
+    private $testUrl = 'https://www.example.com/path/file.html?param1=value1&param2%5Bsub1%5D=valueSub1&param2%5Bsub2%5D=valueSub2';
 
     public function testBuildUrlWithArrayOrParameters()
     {
-        $htmlHelper = new UrlHelper();
+        $urlHelper = new UrlHelper();
 
         $components = [
             'scheme' => 'https',
@@ -17,31 +17,31 @@ class UrlHelperTest extends \PHPUnit_Framework_TestCase
             'path' => '/path/file.html',
             'query' => ['param1' => 'value1', 'param2' => ['sub1' => 'valueSub1', 'sub2' => 'valueSub2']]
         ];
-        $result = $htmlHelper->buildUrl($components);
+        $result = $urlHelper->buildUrl($components);
 
         $this->assertEquals($this->testUrl, $result);
     }
 
     public function testBuildUrlWithStringParameters()
     {
-        $htmlHelper = new UrlHelper();
+        $urlHelper = new UrlHelper();
 
         $components = [
             'scheme' => 'https',
             'host' => 'www.example.com',
             'path' => '/path/file.html',
-            'query' => 'param1=value1&param2[sub1]=valueSub1&param2[sub2]=valueSub2'
+            'query' => 'param1=value1&param2%5Bsub1%5D=valueSub1&param2%5Bsub2%5D=valueSub2'
         ];
-        $result = $htmlHelper->buildUrl($components);
+        $result = $urlHelper->buildUrl($components);
 
         $this->assertEquals($this->testUrl, $result);
     }
 
     public function testParseUrlParseQueryString()
     {
-        $htmlHelper = new UrlHelper();
+        $urlHelper = new UrlHelper();
 
-        $result = $htmlHelper->parseUrl($this->testUrl);
+        $result = $urlHelper->parseUrl($this->testUrl);
 
         $this->assertInternalType('array', $result['query']);
         $this->assertArrayHasKey('param1', $result['query']);
@@ -50,48 +50,48 @@ class UrlHelperTest extends \PHPUnit_Framework_TestCase
 
     public function testUpdateUrlUpdatesNothingWithEmptyComponents()
     {
-        $htmlHelper = new UrlHelper();
+        $urlHelper = new UrlHelper();
 
-        $result = $htmlHelper->updateUrl($this->testUrl, []);
+        $result = $urlHelper->updateUrl($this->testUrl, []);
 
         $this->assertEquals($this->testUrl, $result);
     }
 
     public function testUpdateUrlUpdatesDomain()
     {
-        $htmlHelper = new UrlHelper();
+        $urlHelper = new UrlHelper();
 
-        $result = $htmlHelper->updateUrl($this->testUrl, ['host' => 'example.cz']);
-        $expected = 'https://example.cz/path/file.html?param1=value1&param2[sub1]=valueSub1&param2[sub2]=valueSub2';
+        $result = $urlHelper->updateUrl($this->testUrl, ['host' => 'example.cz']);
+        $expected = 'https://example.cz/path/file.html?param1=value1&param2%5Bsub1%5D=valueSub1&param2%5Bsub2%5D=valueSub2';
 
         $this->assertEquals($expected, $result);
     }
 
     public function testUpdateUrlUpdatesParameter()
     {
-        $htmlHelper = new UrlHelper();
+        $urlHelper = new UrlHelper();
 
-        $result = $htmlHelper->updateUrl($this->testUrl, ['query' => ['param1' => 'anotherValue']]);
-        $expected = 'https://www.example.com/path/file.html?param1=anotherValue&param2[sub1]=valueSub1&param2[sub2]=valueSub2';
+        $result = $urlHelper->updateUrl($this->testUrl, ['query' => ['param1' => 'anotherValue']]);
+        $expected = 'https://www.example.com/path/file.html?param1=anotherValue&param2%5Bsub1%5D=valueSub1&param2%5Bsub2%5D=valueSub2';
 
         $this->assertEquals($expected, $result);
     }
 
     public function testUpdateUrlUpdatesNestedParameter()
     {
-        $htmlHelper = new UrlHelper();
+        $urlHelper = new UrlHelper();
 
-        $result = $htmlHelper->updateUrl($this->testUrl, ['query' => ['param2' => ['sub1' => 'anotherSubParameter']]]);
-        $expected = 'https://www.example.com/path/file.html?param1=value1&param2[sub1]=anotherSubParameter&param2[sub2]=valueSub2';
+        $result = $urlHelper->updateUrl($this->testUrl, ['query' => ['param2' => ['sub1' => 'anotherSubParameter']]]);
+        $expected = 'https://www.example.com/path/file.html?param1=value1&param2%5Bsub1%5D=anotherSubParameter&param2%5Bsub2%5D=valueSub2';
 
         $this->assertEquals($expected, $result);
     }
 
     public function testUpdateUrlRemovesParameter()
     {
-        $htmlHelper = new UrlHelper();
+        $urlHelper = new UrlHelper();
 
-        $result = $htmlHelper->updateUrl($this->testUrl, ['query' => ['param2' => null]]);
+        $result = $urlHelper->updateUrl($this->testUrl, ['query' => ['param2' => null]]);
         $expected = 'https://www.example.com/path/file.html?param1=value1';
 
         $this->assertEquals($expected, $result);
