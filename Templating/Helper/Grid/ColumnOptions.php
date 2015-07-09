0dc65ad50504362218cd3426ef706f626b0f8615
Pavel Batecko <pavel.batecko@imatic.cz>
2015-07-09 17:26:12 +0200

    ColumnOptions - added humanization to the default label

--- Templating/Helper/Grid/ColumnOptions.php
+++ Templating/Helper/Grid/ColumnOptions.php
@@ -3,6 +3,7 @@
 namespace Imatic\Bundle\ViewBundle\Templating\Helper\Grid;
 
 use Imatic\Bundle\ViewBundle\Templating\Utils\AbstractOptions;
+use Imatic\Bundle\ViewBundle\Templating\Utils\String;
 use Symfony\Component\OptionsResolver\Options;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
@@ -26,11 +27,11 @@ class ColumnOptions extends AbstractOptions
             'class' => '',
             'sortable' => false,
             'label' => function (Options $options) {
-                    return $options['name'];
-                },
+                return String::humanize($options['name']);
+            },
             'propertyPath' => function (Options $options) {
-                    return $options['name'];
-                }
+                return $options['name'];
+            }
         ]);
         $resolver->setAllowedTypes([
             'name' => 'string',
