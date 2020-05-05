19de95873c4ae83292302014f6ff4a3b7de7b7fe
Lukas Landa <landa.lukas@gmail.com>
2020-05-05 20:36:18 +0200

    Migration to bootstrap 4

--- Resources/assets/js/select2/select2-focus-war-fix.js
+++
@@ -1,7 +0,0 @@
-// https://github.com/select2/select2/pull/2176
-// (bug most likely reintroduced in v4.0.0)
-$(function () {
-    $('body').on('focus', '.select2-dropdown', function (e) {
-        e.stopPropagation();
-    });
-});
